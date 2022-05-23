c835a730d341ed4dc1baf3f79a7b9b43808a7ee6
Bruno Gomes de Araujo <2729795@ifrn.local>
2022-05-23 16:12:18 -0300

    Configuracao de Templates

--- coapac/settings.py
+++ coapac/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'coapac.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
