df4b430f490e81970a683ce70c0bbe6f6cdbbd04
Allane <allane.costa@escolar.ifrn.edu.br>
2022-06-29 11:40:31 -0300

    atualização do projeto no django

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-def home(request):
+def index(request):
     return render(request, 'index.html')
+
+def emprestimo(request):
+    return render(request, 'emprestimo.html')
+
+def devolucao(request):
+    return render(request, 'devolucao.html')
+
+def listar(request):
+    return render(request, 'listar.html')
+
+def novo(request):
+    return render(request, 'novo.html')
--- coapac/urls.py
+++ coapac/urls.py
@@ -13,11 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import imp
 from django.contrib import admin
 from django.urls import path
-from apps.core.views import home
+from apps.core.views import index, emprestimo, devolucao, listar, novo
+
 
 urlpatterns = [
-    path('', home),
+    path('', index, name="inicio"),
     path('admin/', admin.site.urls),
+    path('emprestimo/',emprestimo, name="emprestimo"),
+    path('devolução/', devolucao, name="devolucao"),
+    path('listar/', listar, name="listar"),
+    path('novo/', novo, name="novo"),
 ]
